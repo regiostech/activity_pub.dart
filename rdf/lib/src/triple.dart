9bea3ee37ebb44f25094127f71d027dcb2a4d99d
Tobe O <thosakwe@gmail.com>
2019-11-22 11:41:34 -0500

    Triple hashes/==

--- rdf/lib/src/triple.dart
+++ rdf/lib/src/triple.dart
@@ -11,11 +11,30 @@ class Triple {
   Uri get predicate => _predicate;
 
   TripleObject get object => _object;
+
+  @override
+  int get hashCode => hash3(_subject, _predicate, _object);
+
+  @override
+  bool operator ==(other) =>
+      other is Triple &&
+      _subject == other._subject &&
+      _predicate == other._predicate &&
+      _object == other._object;
 }
 
 class TripleSubject {
   Uri _iri;
   Object _blankNode;
+
+  @override
+  int get hashCode => hash2(_iri, _blankNode);
+
+  @override
+  bool operator ==(other) =>
+      other is TripleSubject &&
+      _iri == other._iri &&
+      _blankNode == other._blankNode;
 }
 
 class TripleObject<T> {
@@ -31,15 +50,16 @@ class TripleObject<T> {
 
   TripleObject.blankNode(this._blankNode);
 
-  @override
-  int get hashCode => hash3(_iri, _literal, _blankNode);
-
   Uri get asIri => _iri;
 
   Literal get asLiteral => _literal;
 
   Object get asBlankNode => _blankNode;
 
+  @override
+  int get hashCode => hash3(_iri, _literal, _blankNode);
+
+  @override
   bool operator ==(other) =>
       other is TripleObject &&
       _iri == other._iri &&
