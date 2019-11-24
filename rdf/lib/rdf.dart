a8cc2f8f87456d23f79f76744d6b70e4f7eab2ed
Tobe O <thosakwe@gmail.com>
2019-11-24 00:20:40 -0500

    Restructure

--- rdf/lib/rdf.dart
+++ rdf/lib/rdf.dart
@@ -1,5 +1,1 @@
-export 'src/data_set.dart';
-export 'src/data_type.dart';
-export 'src/graph.dart';
-export 'src/literal.dart';
-export 'src/triple.dart';
+export 'src/rdf/rdf.dart';
---
+++ rdf/lib/src/rdf/rdf.dart
@@ -0,0 +1,5 @@
+export 'data_set.dart';
+export 'data_type.dart';
+export 'graph.dart';
+export 'literal.dart';
+export 'triple.dart';
