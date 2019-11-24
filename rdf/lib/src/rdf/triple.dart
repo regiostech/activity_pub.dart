789e32671c7d6768dc691b8e8e88985f062377a5
Tobe O <thosakwe@gmail.com>
2019-11-24 01:04:24 -0500

    Generalize triples

--- rdf/lib/src/rdf/triple.dart
+++ rdf/lib/src/rdf/triple.dart
@@ -10,89 +10,36 @@ class BlankNode {
   String toString() => '<blank node>';
 }
 
+/// A generalized RDF triple.
 class Triple {
-  TripleSubject _subject;
-  Uri _predicate;
-  TripleObject _object;
+  TripleObject subject;
+  TripleObject predicate;
+  TripleObject object;
 
   factory Triple.fromIterable(Iterable it) {
     return Triple.from(it.elementAt(0), it.elementAt(1), it.elementAt(2));
   }
 
-  Triple(this._subject, this._predicate, this._object);
+  Triple(this.subject, this.predicate, this.object);
 
   Triple.from(subject, predicate, object) {
-    _subject = TripleSubject(subject);
-    _object = TripleObject(object);
-    if (predicate is Uri) {
-      _predicate = predicate;
-    } else if (predicate is String) {
-      _predicate = Uri.parse(predicate);
-    } else {
-      throw ArgumentError.value(
-          predicate, 'predicate', 'must be a Uri or String');
-    }
+    this.subject = TripleObject(subject);
+    this.predicate = TripleObject(predicate);
+    this.object = TripleObject(object);
   }
 
-  TripleSubject get subject => _subject;
-
-  Uri get predicate => _predicate;
-
-  TripleObject get object => _object;
-
   @override
-  int get hashCode => hash3(_subject, _predicate, _object);
+  int get hashCode => hash3(subject, predicate, object);
 
   @override
   bool operator ==(other) =>
       other is Triple &&
-      _subject == other._subject &&
-      _predicate == other._predicate &&
-      _object == other._object;
+      subject == other.subject &&
+      predicate == other.predicate &&
+      object == other.object;
 
   @override
-  String toString() => '$_subject <$_predicate> $_object .';
-}
-
-class TripleSubject {
-  Uri _iri;
-  BlankNode _blankNode;
-
-  factory TripleSubject(obj) {
-    if (obj is Uri) {
-      return TripleSubject.iri(obj);
-    } else if (obj is BlankNode) {
-      return TripleSubject.blankNode(obj);
-    } else {
-      throw ArgumentError.value(obj, 'subject', 'must be Uri or BlankNode');
-    }
-  }
-
-  Uri get asIri => _iri;
-
-  BlankNode get asBlankNode => _blankNode;
-
-  TripleSubject.iri(this._iri);
-
-  TripleSubject.blankNode(this._blankNode);
-
-  @override
-  int get hashCode => hash2(_iri, _blankNode);
-
-  @override
-  bool operator ==(other) =>
-      other is TripleSubject &&
-      _iri == other._iri &&
-      _blankNode == other._blankNode;
-
-  @override
-  String toString() {
-    if (_iri != null) {
-      return '<$_iri>';
-    } else {
-      return _blankNode.toString();
-    }
-  }
+  String toString() => '$subject $predicate $object .';
 }
 
 class TripleObject<T> {
