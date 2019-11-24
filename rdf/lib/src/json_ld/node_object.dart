b2f112780d766a2c52f01b1f144a71245dda84d3
Tobe O <thosakwe@gmail.com>
2019-11-24 01:45:37 -0500

    NodeObject

--- rdf/lib/src/json_ld/node_object.dart
+++ rdf/lib/src/json_ld/node_object.dart
@@ -2,9 +2,14 @@ import 'context_definition.dart';
 import 'list_or.dart';
 import 'uri_or.dart';
 
+// https://www.w3.org/TR/json-ld/#dfn-node-object
 class NodeObject {
   ListOr<UriOr<ContextDefinition>> context;
   Uri id;
-  List<NodeObject> graphs;
+  ListOr<NodeObject> graph;
+  ListOr<UriOr<String>> type;
+  Map<String, ListOr> reverse;
+  String index;
+  Map<String, dynamic> otherValues;
   NodeObject({this.context, this.id});
 }
